45750ead1f9e35253ea868a0facbc0d35f10b126
Jack Betteridge <J.D.Betteridge@bath.ac.uk>
2018-08-31 16:36:33 +0100

    Made a thing to render plain notebooks

--- tools/nbfancy.py
+++ tools/nbfancy.py
@@ -7,6 +7,8 @@ import pprint
 import nbformat as nf
 import nbconvert as nc
 
+from urllib.parse import quote as urlquote
+
 def isdir(path):
     if not os.path.isdir(path):
         raise OSError('"' + path + '"' + ' is not a direcotry')
@@ -19,7 +21,7 @@ parser.add_argument('input',
                     help='Plain input notebook')
                     
 parser.add_argument('output',
-                    type=argparse.FileType('w'),
+                    type=str,
                     help='Name of fancy notebook to output')
                     
 parser.add_argument('--headercell',
@@ -37,17 +39,16 @@ args = parser.parse_args()
 
 def apply_template(colour, symbol, title, body):
     template = '''
-<div class="text_cell_render border-box-sizing rendered_html">
-<div class="w3-panel w3-leftbar w3-border-%(COLOUR)s w3-pale-%(COLOUR)s w3-padding-small">
-    <h3><i class="fa fa-%(SYMBOL)s"></i> %(TITLE)s</h3>
-    %(BODY)s
-</div>
+<div class="w3-panel w3-leftbar w3-border-{colour} w3-pale-{colour} w3-padding-small">
+    <h3><i class="fa fa-{symbol}"></i> {title}</h3>
+    {body}
 </div>
 '''
-    return template%{   'COLOUR' : colour,
-                        'SYMBOL' : symbol,
-                        'TITLE'  : title,
-                        'BODY'   : body}
+    values = {  'colour' : colour,
+                'symbol' : symbol,
+                'title'  : title,
+                'body'   : body}
+    return template.format_map(values)
 
 # Colours
 # ~ green
@@ -62,11 +63,22 @@ def apply_template(colour, symbol, title, body):
 # ~ eye
 # ~ key
 
+solnfilename = args.output.replace('.ipynb', '-soln.ipynb')
+solnflag = False
+
 print('Reading input file: ' + args.input.name)
 
 plain = nf.read(args.input, nf.NO_CONVERT)
 celllist = plain['cells']
 markdownlist = [c for c in celllist if c['cell_type']=='markdown']
+
+solnb = None
+
+if args.headercell is not None:
+    print('Reading from headercell: ' + args.headercell.name)
+    header = nf.read(args.headercell, nf.NO_CONVERT)
+    plain['cells'].insert(0, *header['cells'])
+    args.headercell.close()
 
 for c in markdownlist:
     line = c['source'].split('\n')
@@ -92,7 +104,22 @@ for c in markdownlist:
         subtitle = line[0].split(':')
         title = ':'.join(subtitle[1:])
         body = '\n'.join(line[1:])
+        safetitle = title.replace(' ', '-')
+        safetitle = safetitle.replace('`', '')
+        link = solnfilename + '#' + urlquote(safetitle, safe='?!$\\') + '%0A'
+        body += '\n\n [Solution]({link})'.format(link=link)
     elif 'Solution' in line[0]:
+        solnflag = True
+        if solnb is None:
+            solnb = nf.v4.new_notebook()
+            solnb['metadata'] = plain['metadata']
+            solnb['cells'].append(nf.v4.new_markdown_cell(source='# Solutions'))
+        
+        solnb['cells'].append(nf.v4.new_markdown_cell(source=''))
+        # REDEFINE c
+        plain['cells'].remove(c)
+        c = solnb['cells'][-1]
+        
         colour = 'blue'
         symbol = 'eye'
         subtitle = line[0].split(':')
@@ -114,25 +141,62 @@ for c in markdownlist:
         colour = None
     
     if colour is not None:
-        c['source'] = apply_template(colour, symbol, title, nc.filters.markdown2html(body))
+        htmltitle = nc.filters.markdown2html(title)
+        temp = htmltitle.replace('<p>', '')
+        htmltitle = temp.replace('</p>', '')
+        htmlbody = nc.filters.markdown2html(body)
+        c['source'] = apply_template(colour, symbol, htmltitle, htmlbody)
 
-if args.headercell is not None:
-    print('Reading from headercell: ' + args.headercell.name)
-    header = nf.read(args.headercell, nf.NO_CONVERT)
-    plain['cells'].insert(0, *header['cells'])
-    args.headercell.close()
+def navigation_triple(directory, inputfile):
+    print('Directory: ', directory)
+    print('contains: ')
+    contents = os.listdir(directory)
+    contents.sort()
+    try:
+        contents.remove('.ipynb_checkpoints')
+    except ValueError:
+        pass
+
+    for afile in contents:
+        print('          ', afile)
     
+    contents.append(contents[0])
+    
+    current = inputfile.split('/')[-1]
+    index = contents.index(current)
+    outdir = './'
+    print('Navigation triple is: ', outdir+contents[index-1], outdir+contents[0], outdir+contents[index+1])
+    triple = {  'previous' : outdir+contents[index-1],
+                'index'    : outdir+contents[0],
+                'next'     : outdir+contents[index+1] }
+    return triple
+
 if args.footercell is not None:
     print('Reading from footercell: ' + args.footercell.name)
     footer = nf.read(args.footercell, nf.NO_CONVERT)
-    plain['cells'].append(*footer['cells'])
+    
+    triple = {'index' : './00_schedule.ipynb'} # Prevent error
+    if args.sourcedir is not None:
+        triple = navigation_triple(args.sourcedir, args.input.name)
+        for cell in footer['cells']:
+            #print(cell['source'].format_map(triple))
+            cell['source'] = cell['source'].format_map(triple)
+    
+    inputname = './' + args.input.name.split('/')[-1]
+    if triple['index'] != inputname:
+        plain['cells'].append(*footer['cells'])
     args.footercell.close()
     
-print('Writing output file: ' + args.output.name)
-#pprint.pprint(plain)
-nf.write(plain, args.output)
+outfp = open(args.output, 'w')
+print('Writing output file: ' + args.output)
+nf.write(plain, outfp)
 args.input.close()
-args.output.close()
+outfp.close()
 
-if args.sourcedir is not None:
-    print('Directory: ', args.sourcedir)
+if solnflag:
+    solfp = open(solnfilename, 'w')
+    print('and also solution outputfile')
+    nf.write(solnb, solfp)
+    solfp.close()
+    
+    
